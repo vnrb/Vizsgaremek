81bd18845b8625609d37064f5d6991b6689ea86f
nrb <mail.vargan@gmail.com>
2024-03-14 08:33:23 +0100

    adatbázis lekérdezés az statisztikai oldalakra kész

--- backend/server.js
+++ backend/server.js
@@ -3,133 +3,32 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-app.use(express.static('../'));
-
-// SERVER KÉRÉSEK
-
-app.get('/', (req, res) =>{
-    fs.readFile("../frontend/html/index.html", (err, file)=>{
-        res.setHeader('access-control-allow-origin','*');
-        res.setHeader('Content-type', 'text/html');
-        res.status(200);
-        res.end(file);
-    });
-});
-
-app.get('/dynasty', (req, res) =>{
-    fs.readFile("../frontend/html/dynasty ranking.html", (err, file)=>{
-        res.setHeader('access-control-allow-origin','*');
-        res.setHeader('Content-type', 'text/html');
-        res.status(200);
-        res.end(file);
-    });
-});
-
-app.get('/redraft', (req, res) =>{
-    fs.readFile("../frontend/html/redraft ranking.html", (err, file)=>{
-        res.setHeader('access-control-allow-origin','*');
-        res.setHeader('Content-type', 'text/html');
-        res.status(200);
-        res.end(file);
-    });
-});
-
-app.get('/rookies', (req, res) =>{
-    fs.readFile("../frontend/html/rookie ranking.html", (err, file)=>{
-        res.setHeader('access-control-allow-origin','*');
-        res.setHeader('Content-type', 'text/html');
-        res.status(200);
-        res.end(file);
-    });
-});
-
-app.get('/injuries', (req, res) =>{
-    fs.readFile("../frontend/html/injuries.html", (err, file)=>{
-        res.setHeader('access-control-allow-origin','*');
-        res.setHeader('Content-type', 'text/html');
-        res.status(200);
-        res.end(file);
-    });
-});
-
-app.get('/blog', (req, res) =>{
-    fs.readFile("../frontend/html/blog.html", (err, file)=>{
-        res.setHeader('access-control-allow-origin','*');
-        res.setHeader('Content-type', 'text/html');
-        res.status(200);
-        res.end(file);
-    });
-});
-
-app.get('/fantasyligak', (req, res) =>{
-    fs.readFile("../frontend/html/fantasyligak.html", (err, file)=>{
-        res.setHeader('access-control-allow-origin','*');
-        res.setHeader('Content-type', 'text/html');
-        res.status(200);
-        res.end(file);
-    });
-});
-
-app.get('/hirek', (req, res) =>{
-    fs.readFile("../frontend/html/hirek.html", (err, file)=>{
-        res.setHeader('access-control-allow-origin','*');
-        res.setHeader('Content-type', 'text/html');
-        res.status(200);
-        res.end(file);
-    });
-});
-
-app.get('/admin', (req, res) =>{
-    fs.readFile("../frontend/html/admin.html", (err, file)=>{
-        res.setHeader('access-control-allow-origin','*');
-        res.setHeader('Content-type', 'text/html');
-        res.status(200);
-        res.end(file);
-    });
-});
-
-app.get('/tippjatek', (req, res) =>{
-    fs.readFile("../frontend/html/tippjatek.html", (err, file)=>{
-        res.setHeader('access-control-allow-origin','*');
-        res.setHeader('Content-type', 'text/html');
-        res.status(200);
-        res.end(file);
-    });
-});
-
 
 // ADATBÁZIS LEKÉRDEZÉS 
 
-/*lekerdezes();
-
 const mysql = require('mysql');
-
-function lekerdezes(){
     
-    const kapcsolat = ()=>{
-        return mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: '',
-            database: 'hotpodtato'
-        });
-    };
-
-    app.get('/dynasty',(req,res)=>{
-        const connection = kapcsolat();
-            connection.query("select * from players",(error, result, fields)=>{
-            if(error)
-                res.send({"error":"Hiba lépett fel a lekérés során."})
-            else
-                res.send(result);    
-                console.log(result);
-                
-                
-        })
-
-        connection.end();
+const kapcsolat = ()=>{
+    return mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'hotpodtato'
     });
+};
 
-}*/
+app.get('/dynasty',(req,res)=>{
+    const connection = kapcsolat();
+        connection.query("select * from players",(error, result, fields)=>{
+        if(error)
+            res.send({"error":"Hiba lépett fel a lekérés során."})
+        else
+            res.setHeader('Access-Control-Allow-Origin','*');
+            res.send(result);    
+            console.log(result);
+    })
+
+    connection.end();
+});
 
 app.listen(3000);
---
+++ frontend/public/js/rookie_lekerdezes.js
@@ -0,0 +1,93 @@
+// ADATBÁZIS LEKÉRDEZÉS:
+
+const CellatASorba = (row, adat)=>{
+    column = document.createElement("td");
+    column.innerHTML = adat;
+    row.appendChild(column);
+};
+
+const FejlecKeszites = (table)=>{
+    const row = document.createElement("tr");
+    row.setAttribute("id", "fejlec")
+    CellatASorba(row,"Player Name");
+    CellatASorba(row,"Team");
+    CellatASorba(row,"Position");
+    CellatASorba(row,"Exp");
+    CellatASorba(row,"Status");
+    CellatASorba(row,"GP");
+    CellatASorba(row,"P-yards");
+    CellatASorba(row,"P-tds");
+    CellatASorba(row,"P-int");
+    CellatASorba(row,"R-yards");
+    CellatASorba(row,"R-tds");
+    CellatASorba(row,"Rec");
+    CellatASorba(row,"RecYards")
+    CellatASorba(row,"Rectds");
+    CellatASorba(row,"Sck");
+    CellatASorba(row,"RecInts");
+    CellatASorba(row,"Ffumbles");
+    CellatASorba(row,"Freturn");
+    CellatASorba(row,"PPG");
+    CellatASorba(row,"Total points");
+    table.appendChild(row);
+};
+
+AdatokBetolteseTablaba =(adatok)=>{
+    const div = document.getElementById("tablaDiv3");
+    const table = document.createElement("table");
+    FejlecKeszites(table);
+    for(let i =0; i<adatok.length; i++)
+    {
+        const row = document.createElement("tr");
+        CellatASorba(row,adatok[i].playername);
+        CellatASorba(row,adatok[i].team);
+        CellatASorba(row,adatok[i].position);
+        CellatASorba(row,adatok[i].exp);
+        CellatASorba(row,adatok[i].status);
+        CellatASorba(row,adatok[i].gamesplayed);
+        CellatASorba(row,adatok[i].pyards);
+        CellatASorba(row,adatok[i].ptds);
+        CellatASorba(row,adatok[i].pint);
+        CellatASorba(row,adatok[i].rushyards);
+        CellatASorba(row,adatok[i].rushtds);
+        CellatASorba(row,adatok[i].rec);
+        CellatASorba(row,adatok[i].recyards);
+        CellatASorba(row,adatok[i].rectds);
+        CellatASorba(row,adatok[i].sck);
+        CellatASorba(row,adatok[i].recints);
+        CellatASorba(row,adatok[i].ffumbles);
+        CellatASorba(row,adatok[i].freturn);
+        CellatASorba(row,adatok[i].ppg);
+        CellatASorba(row,adatok[i].totalpoint);
+
+        table.appendChild(row);
+    } 
+    div.appendChild(table);
+
+
+}
+
+const AdatokLekereseAdatbazisbol = ()=>{
+    let adatok =[];
+    fetch("http://127.0.0.1:3000/dynasty")
+    .then((res)=>{
+        if(!res.ok)
+            console.log("Hiba lépett fel az adatbázis kapcsolat létrehozása során");
+        else
+            return res.json();
+
+    })
+    .then((res)=>{
+        res.forEach(element =>{
+            adatok.push(element)
+        });
+        AdatokBetolteseTablaba(adatok);
+        console.log(adatok);
+    });
+};
+
+const Main = ()=>{
+    AdatokLekereseAdatbazisbol();
+};
+
+Main();
